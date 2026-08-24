1c939421fd704063fcd63591f09eb5fa64efda17
packletka <pavel.azyava@gmail.com>
2026-08-24 12:03:38 +0300

    FEATURE: added support for authentication using the jwt token in settings.py and urls.py , coordination with the admin panel

--- backend/talonby/urls.py
+++ backend/talonby/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-# from backend.testapp.urls import router
-
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/test", include("testapp.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/test", include("testapp.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+]
