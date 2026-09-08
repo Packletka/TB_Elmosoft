9c43252f7cb8ede946c6520e541597300a759778
packletka <pavel.azyava@gmail.com>
2026-09-08 09:32:11 +0300

    BACKEND REFACTOR: improve talon serializer response shape

--- backend/appointments/serializers.py
+++ backend/appointments/serializers.py
@@ -7,36 +7,74 @@ from .models import Talons
 from .utils import validate_appointment
 
 
-class HealthOrganisationSerializer(ModelSerializer):
+class TalonHealthOrganisationSerializer(ModelSerializer):
     class Meta:
         model = HealthOrganisation
-        fields = ("name", "address", "phone", "email")
+        fields = (
+            "id",
+            "name",
+            "address",
+            "phone",
+            "email",
+            "site",
+        )
+
+
+class TalonDoctorSerializer(ModelSerializer):
+    full_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    patronymic = serializers.CharField(source="user.patronymic", read_only=True)
+    health_organisation = TalonHealthOrganisationSerializer(read_only=True)
+
+    class Meta:
+        model = Doctor
+        fields = (
+            "id",
+            "full_name",
+            "last_name",
+            "first_name",
+            "patronymic",
+            "position",
+            "cabinet",
+            "health_organisation",
+        )
+
+    def get_full_name(self, obj):
+        name_parts = [
+            obj.user.last_name,
+            obj.user.first_name,
+            obj.user.patronymic,
+        ]
+
+        return " ".join(part for part in name_parts if part)
 
 
 class TalonsSerializer(ModelSerializer):
     customer = serializers.PrimaryKeyRelatedField(read_only=True)
-    doctor = serializers.PrimaryKeyRelatedField(
+    doctor = TalonDoctorSerializer(read_only=True)
+    doctor_id = serializers.PrimaryKeyRelatedField(
+        source="doctor",
         queryset=Doctor.objects.all(),
         write_only=True,
         required=True,
     )
-
-    doctor_info = serializers.SerializerMethodField(read_only=True)
+    is_free = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Talons
-        fields = ("id", "customer", "doctor", "doctor_info", "date", "time")
+        fields = (
+            "id",
+            "customer",
+            "doctor",
+            "doctor_id",
+            "date",
+            "time",
+            "is_free",
+        )
 
-    def get_doctor_info(self, obj):
-        return {
-            "id": obj.doctor.id,
-            "position": obj.doctor.position,
-            "health_organisation": HealthOrganisationSerializer(
-                obj.doctor.health_organisation,
-            ).data
-            if obj.doctor.health_organisation
-            else None,
-        }
+    def get_is_free(self, obj):
+        return obj.customer_id is None
 
     def validate(self, data):
         doctor = data.get("doctor", self.instance.doctor if self.instance else None)
